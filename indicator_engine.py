8cc4378a07e1542064affae9d136ee7e8c6899d6
RuedaJ <jean.rueda.w@gmail.com>
2025-07-20 17:52:59 +0200

    Add files via upload

--- indicator_engine.py
+++ indicator_engine.py
@@ -2,18 +2,15 @@
 from indicators.landcover import LandCoverIndicator
 from indicators.power_demand import PowerDemandIndicator
 from indicators.population_density import PopulationDensityIndicator
+from indicators.flood_risk import FloodRiskIndicator
 
 def run_indicators(gdf):
-    """
-    Apply all configured indicators to the input GeoDataFrame.
-    """
     indicators = [
         LandCoverIndicator(),
         PowerDemandIndicator(),
-        PopulationDensityIndicator()
+        PopulationDensityIndicator(),
+        FloodRiskIndicator()
     ]
-
     for indicator in indicators:
         gdf = indicator.extract(gdf)
-
     return gdf
